f6b086390cdc765b6f05d611e4ef54b6c65b0d29
Lauren Tyson <lauren.wlkr@gmail.com>
2022-08-24 17:47:53 -0600

    Change header image and put inline with header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import foodie from "../images/Foodie.png"
 
 function Header() {
   return (
-    <div className="header">
-      <h1>Scientific Foodie</h1>
+    <div className="header">        
+      <h1>Scientific {<img src={foodie}/>}</h1>
     </div>
   );
 }
