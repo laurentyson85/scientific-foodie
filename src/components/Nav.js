c68f2c8e6a439411d2eef84fa15172da36601363
Lauren Tyson <lauren.wlkr@gmail.com>
2022-08-24 16:58:47 -0600

    Add nav bar styling in CSS

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom"
 
 function Nav() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/foods">Foods</Link>
-      <Link to="/form">Submit New Food</Link>
-    </nav>
+    <div className="navBar">
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/foods">Foods</Link>
+        <Link to="/form">Submit New Food</Link>
+        </nav>
+    </div>
+    
   );
 }
 
