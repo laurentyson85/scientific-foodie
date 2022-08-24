48e3380292cc375f23278dd41ebb2fc83346de00
Lauren Tyson <lauren.wlkr@gmail.com>
2022-08-24 16:23:06 -0600

    Add Home component to App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom"
 import Header from "./Header";
 import Nav from "./Nav";
+import Home from "./Home";
 import FoodContainer from "./FoodContainer";
 import FoodForm from "./FoodForm";
 
