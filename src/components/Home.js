a511b6b0c425cf818cbb5ae605cae31c76c855a4
Lauren Tyson <lauren.wlkr@gmail.com>
2022-08-25 09:01:16 -0600

    Add Css styling to Home component text

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="home">
       <h2>Welcome to the database that shares scientific information about fruits, vegatables, grains, gourds, and more!</h2>
-      <p>Use the navigation at the top to view and learn about different foods, and to submit a new food to the database.</p>
+      <p>Use the navigation at the top to view and learn about different foods and to submit a new food to the database.</p>
       <p>Recently added food:</p>
 
     </div>
@@ -13,3 +13,5 @@ function Home() {
 }
 
 export default Home;
+
+//have the state that contains the newly submitted food default to the last food in the db.json
