f85efe8b5d49bd66b1360b95cc84d2134599e78d
Lauren Tyson <lauren.wlkr@gmail.com>
2022-08-25 10:09:29 -0600

    Write FoodContainer JSX to get data on page

--- src/components/FoodContainer.js
+++ src/components/FoodContainer.js
@@ -1,14 +1,20 @@
 import React from "react";
-
+import FoodCard from "./FoodCard";
 
 function FoodContainer({foods}) {
- 
 
+    const myFoods = foods.map(food => {
+        return(
+            <FoodCard
+            key={food.id}
+            scientificName={food.scientificName}
+            image={food.image}
+            />
+        )
+    }) 
 
   return (
-    <div>
-      <h1>Foods Here!</h1>
-    </div>
+    <ul className="cards">{myFoods}</ul>
   );
 }
 
