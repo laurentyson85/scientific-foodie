9beaae5645f263c2d4d72d0eb6489bcd670c21e6
Lauren Tyson <lauren.wlkr@gmail.com>
2022-08-25 08:39:22 -0600

    Add Home component language

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,10 @@ import React from "react";
 function Home() {
   return (
     <div className="home">
-      <h1>Home!</h1>
+      <h2>Welcome to the database that shares scientific information about fruits, vegatables, grains, gourds, and more!</h2>
+      <p>Use the navigation at the top to view and learn about different foods, and to submit a new food to the database.</p>
+      <p>Recently added food:</p>
+
     </div>
   );
 }
